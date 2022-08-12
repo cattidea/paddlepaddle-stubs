e5ee534d30e0244638b5bc8dc5cc1f65a3ab114b
SigureMo <sigure.qaq@gmail.com>
2022-08-13 04:35:04 +0800

    🏷️ types: update paddle.Tensor

--- examples/mnist_example.py
+++ examples/mnist_example.py
@@ -30,10 +30,10 @@ model.load("output/mnist")
 
 # 从测试集中取出一张图片
 img, label = test_dataset[0]
-# 将图片shape从1*28*28变为1*1*28*28，增加一个batch维度，以匹配模型输入格式要求
-img_batch = np.expand_dims(img.astype("float32"), axis=0)
+# 将图片 shape 从 1*28*28 变为 1*1*28*28，增加一个 batch 维度，以匹配模型输入格式要求
+img_batch = np.expand_dims(img.astype("float32"), axis=0)  # type: ignore
 
-# 执行推理并打印结果，此处predict_batch返回的是一个list，取出其中数据获得预测结果
+# 执行推理并打印结果，此处 predict_batch 返回的是一个 list，取出其中数据获得预测结果
 out = model.predict_batch(img_batch)[0]
 pred_label = out.argmax()
 print("true label: {}, pred label: {}".format(label[0], pred_label))
---
+++ tests/test_Tensor.py
@@ -0,0 +1,38 @@
+# pyright: strict, reportUnusedVariable=false
+
+from __future__ import annotations
+
+import paddle
+from paddle import Tensor
+from typing_extensions import assert_type
+
+
+def test_import():
+    paddle.Tensor
+
+    from paddle import Tensor  # pyright: ignore [reportUnusedImport]
+
+
+def test_creation():
+    tensor = paddle.to_tensor([1])
+    assert_type(tensor, Tensor)
+
+
+def test_methods():
+    a = paddle.to_tensor([1])
+    b = paddle.to_tensor([2])
+
+    c = a + b
+    d = a - b
+    e = a * b
+    f = a / b
+    g = a**b
+    h = a // b
+    i = a % b
+    j = len(a)
+    k = iter(a)
+    l = a[1]
+    m = a[:1]
+    n = a[b]
+
+    # TODO: more methods
