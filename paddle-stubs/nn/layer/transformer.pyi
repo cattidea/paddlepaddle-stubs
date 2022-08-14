d16321736b539c20a302d1f78160d2731ade36ff
SigureMo <sigure.qaq@gmail.com>
2022-08-15 02:03:42 +0800

    🏷️ types: paddle.nn.layer.transformer.*

--- paddle-stubs/nn/layer/transformer.pyi
+++ paddle-stubs/nn/layer/transformer.pyi
@@ -1,21 +1,24 @@
 from __future__ import annotations
 
-from collections import namedtuple
-from typing import Any, Optional
+from typing import Any, NamedTuple, Optional, Sequence
 
 from ... import tensor as tensor
-from ...fluid import layers as layers
-from ...framework import ParamAttr as ParamAttr
+from ..._typing import Tensor
+from ...framework import ParamAttr
 from .. import Layer as Layer
 from .. import LayerList as LayerList
 from .common import Dropout as Dropout
 from .common import Linear as Linear
-from .norm import LayerNorm as LayerNorm
+from .norm import LayerNorm
 
 class MultiHeadAttention(Layer):
-    Cache = namedtuple("Cache", ["k", "v"])
+    class Cache(NamedTuple):
+        k: Tensor
+        v: Tensor
 
-    StaticCache = namedtuple("StaticCache", ["k", "v"])
+    class StaticCache(NamedTuple):
+        k: Tensor
+        v: Tensor
     embed_dim: Any = ...
     kdim: Any = ...
     vdim: Any = ...
@@ -29,25 +32,24 @@ class MultiHeadAttention(Layer):
     out_proj: Any = ...
     def __init__(
         self,
-        embed_dim: Any,
-        num_heads: Any,
+        embed_dim: int,
+        num_heads: int,
         dropout: float = ...,
-        kdim: Optional[Any] = ...,
-        vdim: Optional[Any] = ...,
+        kdim: Optional[int] = ...,
+        vdim: Optional[int] = ...,
         need_weights: bool = ...,
-        weight_attr: Optional[Any] = ...,
-        bias_attr: Optional[Any] = ...,
+        weight_attr: Optional[ParamAttr] = ...,
+        bias_attr: Optional[ParamAttr] = ...,
     ) -> None: ...
-    def compute_kv(self, key: Any, value: Any): ...
-    def gen_cache(self, key: Any, value: Optional[Any] = ..., type: Any = ...): ...
     def forward(
         self,
-        query: Any,
-        key: Optional[Any] = ...,
-        value: Optional[Any] = ...,
-        attn_mask: Optional[Any] = ...,
-        cache: Optional[Any] = ...,
-    ): ...
+        query: Tensor,
+        key: Optional[Tensor] = ...,
+        value: Optional[Tensor] = ...,
+        attn_mask: Optional[Tensor] = ...,
+        cache: Optional[MultiHeadAttention.Cache | MultiHeadAttention.StaticCache] = ...,
+    ) -> Tensor | tuple[Tensor, Tensor] | tuple[Tensor, Tensor, Tensor]: ...
+    __call__ = forward
 
 class TransformerEncoderLayer(Layer):
     normalize_before: Any = ...
@@ -62,27 +64,42 @@ class TransformerEncoderLayer(Layer):
     activation: Any = ...
     def __init__(
         self,
-        d_model: Any,
-        nhead: Any,
-        dim_feedforward: Any,
+        d_model: int,
+        nhead: int,
+        dim_feedforward: int,
         dropout: float = ...,
         activation: str = ...,
-        attn_dropout: Optional[Any] = ...,
-        act_dropout: Optional[Any] = ...,
+        attn_dropout: Optional[float] = ...,
+        act_dropout: Optional[float] = ...,
         normalize_before: bool = ...,
-        weight_attr: Optional[Any] = ...,
-        bias_attr: Optional[Any] = ...,
+        weight_attr: Optional[ParamAttr | Sequence[ParamAttr]] = ...,
+        bias_attr: Optional[ParamAttr | Sequence[ParamAttr] | bool] = ...,
     ) -> None: ...
-    def forward(self, src: Any, src_mask: Optional[Any] = ..., cache: Optional[Any] = ...): ...
-    def gen_cache(self, src: Any): ...
+    def forward(
+        self,
+        src: Tensor,
+        src_mask: Optional[Tensor] = ...,
+        cache: Optional[Tensor] = ...,
+    ) -> Tensor | tuple[Tensor, Tensor]: ...
+    __call__ = forward
 
 class TransformerEncoder(Layer):
     layers: Any = ...
     num_layers: Any = ...
     norm: Any = ...
-    def __init__(self, encoder_layer: Any, num_layers: Any, norm: Optional[Any] = ...) -> None: ...
-    def forward(self, src: Any, src_mask: Optional[Any] = ..., cache: Optional[Any] = ...): ...
-    def gen_cache(self, src: Any): ...
+    def __init__(
+        self,
+        encoder_layer: Layer,
+        num_layers: int,
+        norm: Optional[LayerNorm] = ...,
+    ) -> None: ...
+    def forward(
+        self,
+        src: Tensor,
+        src_mask: Optional[Tensor] = ...,
+        cache: Optional[list[MultiHeadAttention.Cache | MultiHeadAttention.StaticCache]] = ...,
+    ) -> Tensor | tuple[Tensor, Tensor]: ...
+    __call__ = forward
 
 class TransformerDecoderLayer(Layer):
     normalize_before: Any = ...
@@ -100,41 +117,46 @@ class TransformerDecoderLayer(Layer):
     activation: Any = ...
     def __init__(
         self,
-        d_model: Any,
-        nhead: Any,
-        dim_feedforward: Any,
+        d_model: int,
+        nhead: int,
+        dim_feedforward: int,
         dropout: float = ...,
         activation: str = ...,
-        attn_dropout: Optional[Any] = ...,
-        act_dropout: Optional[Any] = ...,
+        attn_dropout: Optional[float] = ...,
+        act_dropout: Optional[float] = ...,
         normalize_before: bool = ...,
-        weight_attr: Optional[Any] = ...,
-        bias_attr: Optional[Any] = ...,
+        weight_attr: Optional[ParamAttr | Sequence[ParamAttr]] = ...,
+        bias_attr: Optional[ParamAttr | Sequence[ParamAttr] | bool] = ...,
     ) -> None: ...
     def forward(
         self,
-        tgt: Any,
-        memory: Any,
-        tgt_mask: Optional[Any] = ...,
-        memory_mask: Optional[Any] = ...,
-        cache: Optional[Any] = ...,
-    ): ...
-    def gen_cache(self, memory: Any): ...
+        tgt: Tensor,
+        memory: Tensor,
+        tgt_mask: Optional[Tensor] = ...,
+        memory_mask: Optional[Tensor] = ...,
+        cache: Optional[tuple[MultiHeadAttention.Cache, ...] | tuple[MultiHeadAttention.StaticCache, ...]] = ...,
+    ) -> Tensor | tuple[Tensor, tuple[MultiHeadAttention.Cache, MultiHeadAttention.StaticCache]]: ...
+    __call__ = forward
 
 class TransformerDecoder(Layer):
     layers: Any = ...
     num_layers: Any = ...
     norm: Any = ...
-    def __init__(self, decoder_layer: Any, num_layers: Any, norm: Optional[Any] = ...) -> None: ...
+    def __init__(
+        self,
+        decoder_layer: Layer,
+        num_layers: int,
+        norm: Optional[LayerNorm] = ...,
+    ) -> None: ...
     def forward(
         self,
-        tgt: Any,
-        memory: Any,
-        tgt_mask: Optional[Any] = ...,
-        memory_mask: Optional[Any] = ...,
-        cache: Optional[Any] = ...,
-    ): ...
-    def gen_cache(self, memory: Any, do_zip: bool = ...): ...
+        tgt: Tensor,
+        memory: Tensor,
+        tgt_mask: Optional[Tensor] = ...,
+        memory_mask: Optional[Tensor] = ...,
+        cache: Optional[list[MultiHeadAttention.Cache] | list[MultiHeadAttention.StaticCache]] = ...,
+    ) -> Tensor | tuple[Tensor, Tensor]: ...
+    __call__ = forward
 
 class Transformer(Layer):
     encoder: Any = ...
@@ -150,20 +172,20 @@ class Transformer(Layer):
         dim_feedforward: int = ...,
         dropout: float = ...,
         activation: str = ...,
-        attn_dropout: Optional[Any] = ...,
-        act_dropout: Optional[Any] = ...,
+        attn_dropout: Optional[float] = ...,
+        act_dropout: Optional[float] = ...,
         normalize_before: bool = ...,
-        weight_attr: Optional[Any] = ...,
-        bias_attr: Optional[Any] = ...,
-        custom_encoder: Optional[Any] = ...,
-        custom_decoder: Optional[Any] = ...,
+        weight_attr: Optional[ParamAttr | Sequence[ParamAttr]] = ...,
+        bias_attr: Optional[ParamAttr | Sequence[ParamAttr] | bool] = ...,
+        custom_encoder: Optional[Layer] = ...,
+        custom_decoder: Optional[Layer] = ...,
     ) -> None: ...
     def forward(
         self,
-        src: Any,
-        tgt: Any,
-        src_mask: Optional[Any] = ...,
-        tgt_mask: Optional[Any] = ...,
-        memory_mask: Optional[Any] = ...,
-    ): ...
-    def generate_square_subsequent_mask(self, length: Any): ...
+        src: Tensor,
+        tgt: Tensor,
+        src_mask: Optional[Tensor] = ...,
+        tgt_mask: Optional[Tensor] = ...,
+        memory_mask: Optional[Tensor] = ...,
+    ) -> Tensor: ...
+    __call__ = forward
---
+++ tests/test_nn/test_layer/test_transformer.py
@@ -0,0 +1,32 @@
+# pyright: strict, reportUnusedVariable=false
+
+from __future__ import annotations
+
+import paddle
+from paddle import Tensor
+from typing_extensions import assert_type
+
+
+def test_import():
+    paddle.nn.Transformer
+
+    from paddle.nn import Transformer  # pyright: ignore [reportUnusedImport]
+
+
+def test_types():
+
+    enc_input = paddle.rand((2, 4, 128))
+    dec_input = paddle.rand((2, 6, 128))
+    enc_self_attn_mask = paddle.rand((2, 2, 4, 4))
+    dec_self_attn_mask = paddle.rand((2, 2, 6, 6))
+    cross_attn_mask = paddle.rand((2, 2, 6, 4))
+    transformer = paddle.nn.Transformer(128, 2, 4, 4, 512)
+    output = transformer(
+        enc_input,
+        dec_input,
+        enc_self_attn_mask,
+        dec_self_attn_mask,
+        cross_attn_mask,
+    )
+    assert_type(transformer, paddle.nn.Transformer)
+    assert_type(output, Tensor)
