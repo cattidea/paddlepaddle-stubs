a6d3d65cf5a851bb99ab4cdaccde7877d8e7949f
SigureMo <sigure.qaq@gmail.com>
2024-03-25 04:05:15 +0800

    🏷️ types: complete `paddle.jit` apis, resolves #34

--- paddle-stubs/jit/__init__.pyi
+++ paddle-stubs/jit/__init__.pyi
@@ -1,10 +1,11 @@
 from __future__ import annotations
 
-from ..base.dygraph import ProgramTranslator as ProgramTranslator
-from ..base.dygraph.jit import load as load
-from ..base.dygraph.jit import not_to_static as not_to_static
-from ..base.dygraph.jit import save as save
-from ..base.dygraph.jit import set_code_level as set_code_level
-from ..base.dygraph.jit import set_verbosity as set_verbosity
+from .api import enable_to_static as enable_to_static
+from .api import ignore_module as ignore_module
+from .api import load as load
+from .api import not_to_static as not_to_static
+from .api import save as save
+from .api import set_code_level as set_code_level
+from .api import set_verbosity as set_verbosity
 from .api import to_static as to_static
 from .translated_layer import TranslatedLayer as TranslatedLayer
--- paddle-stubs/jit/api.pyi
+++ paddle-stubs/jit/api.pyi
@@ -1,15 +1,27 @@
 from __future__ import annotations
 
 from collections.abc import Callable
-from typing import Any, TypeVar
+from types import ModuleType
+from typing import Any, TypedDict, TypeVar
 
-from typing_extensions import Literal, ParamSpec, TypeAlias
+from typing_extensions import Literal, ParamSpec, TypeAlias, Unpack
 
 from paddle.static import BuildStrategy, InputSpec
+
+from .translated_layer import TranslatedLayer
 
 _RetT = TypeVar("_RetT")
 _InputT = ParamSpec("_InputT")
 Backends: TypeAlias = Literal["CINN"]
+
+class _SaveLoadConfig(TypedDict):
+    output_spec: Any
+    with_hook: Any
+    combine_params: Any
+    clip_extra: Any
+    skip_forward: Any
+    input_names_after_prune: Any
+    skip_prune_program: Any
 
 def to_static(
     function: Callable[_InputT, _RetT],
@@ -19,3 +31,14 @@ def to_static(
     **kwargs: Any,
 ) -> Callable[_InputT, _RetT]: ...
 def not_to_static(func: Callable[_InputT, _RetT] | None = None) -> Callable[_InputT, _RetT]: ...
+def enable_to_static(enable_to_static_bool: bool) -> None: ...
+def ignore_module(modules: list[ModuleType]) -> None: ...
+def set_code_level(level: int = 100, also_to_stdout: bool = False) -> None: ...
+def set_verbosity(level: int = 0, also_to_stdout: bool = False) -> None: ...
+def save(
+    layer: Callable[_InputT, _RetT],
+    path: str,
+    input_spec: InputSpec | None = None,
+    **configs: Unpack[_SaveLoadConfig],
+) -> None: ...
+def load(path: str, **configs: Unpack[_SaveLoadConfig]) -> TranslatedLayer: ...
